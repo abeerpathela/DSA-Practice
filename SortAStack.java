41fb0a3494a5f7d64a68c9e71cb3d72ca5e888f2
abeerpathela <pathelaabeer@gmail.com>
2026-03-09 16:45:32 +0530

    SortAStack.java added

---
+++ SortAStack.java
@@ -0,0 +1,32 @@
+// sort a stack 
+import java.util.Stack;
+import java.util.*;
+public class Main
+{
+    public static Stack<Integer> sortStack(Stack<Integer> stack){
+        Stack<Integer>temp=new Stack<>();
+        while(!stack.isEmpty()){
+            int current=stack.pop();
+            while(!temp.isEmpty() && temp.peek()<current){
+                stack.push(temp.pop());
+            }
+            temp.push(current);
+        }
+        return temp;
+    }
+	public static void main(String[] args) {
+		Stack<Integer>stack=new Stack<>();
+		
+		stack.push(4);
+		stack.push(1);
+		stack.push(3);
+		stack.push(2);
+		
+		Stack<Integer>answer=sortStack(stack);
+		Iterator<Integer>it=answer.iterator();
+		while(it.hasNext()){
+		    int current=it.next();
+		    System.out.println(current);
+		}
+	}
+}
