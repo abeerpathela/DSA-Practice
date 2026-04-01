7bca5f705926523bfb58d138c1997a4582a52aa9
Abeer Pathela <pathelaabeer@gmail.com>
2026-04-01 17:11:07 +0530

    Time: 5 ms (66.27%), Space: 46.9 MB (64.61%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,29 +1,29 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        Stack<Integer> st= new Stack<>();
+        Stack<Integer> st= new Stack<>(); // this will store the asteroids which are still active and didnt get destroyed 
 
         for(int i=0; i<asteroids.length; i++){
 
             boolean destroyed=false;
 
-            while(!st.isEmpty() && st.peek()>0 && asteroids[i]<0){
-
-                if(Math.abs(st.peek())<Math.abs(asteroids[i])){
+            while(!st.isEmpty() && st.peek()>0 && asteroids[i]<0){  // this loop will make the asteroids collide 
+// one asteroid can destroy many asteroids so thats why we used while loop 
+                if(Math.abs(st.peek())<Math.abs(asteroids[i])){ // this will destroy the stack  asteroid  only 
                     st.pop();
                 }
-                else if(Math.abs(st.peek()) == Math.abs(asteroids[i])){
+                else if(Math.abs(st.peek()) == Math.abs(asteroids[i])){   // in this case both the asteroid got destroyed 
                     st.pop();
                     destroyed=true;
                     break;
                 }
-                else{
+                else{ // this case when the current asteroid got broken but not the stack one asteroid 
                     destroyed=true;
                     break;
                 }
 
             }
 
-            if(!destroyed){
+            if(!destroyed){  // this will push only the asteroids which did not get destroyed 
                 st.push(asteroids[i]);
             }
 
