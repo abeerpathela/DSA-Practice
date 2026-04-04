5ec9d34658761a71bd190d907ddaa50c2e1811dd
Abeer Pathela <pathelaabeer@gmail.com>
2026-04-04 11:09:58 +0530

    Time: 25 ms (63.31%), Space: 56.3 MB (98.22%) - LeetHub

---
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String decodeCiphertext(String encodedText, int rows) {
+        if (rows == 0) return "";
+        
+        int n = encodedText.length();
+        int cols = n / rows;
+        
+        StringBuilder result = new StringBuilder();
+        
+        // Traverse starting from each column in first row
+        for (int startCol = 0; startCol < cols; startCol++) {
+            int i = 0;
+            int j = startCol;
+            
+            while (i < rows && j < cols) {
+                result.append(encodedText.charAt(i * cols + j));
+                i++;
+                j++;
+            }
+        }
+        
+        // Remove trailing spaces
+        int end = result.length() - 1;
+        while (end >= 0 && result.charAt(end) == ' ') {
+            end--;
+        }
+        
+        return result.substring(0, end + 1);
+    }
+}
