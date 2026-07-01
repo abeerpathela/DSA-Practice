1a39b0ca9c5e6baba86724a385168ea991847e95
Abeer Pathela <pathelaabeer@gmail.com>
2026-07-01 18:03:34 +0530

    Time: 295 ms (27.12%), Space: 296.5 MB (20.02%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -18,6 +18,22 @@ public:
         dp[curr][prev+1]=finalAns;
         return dp[curr][prev+1];
     }
+    int solveUsingTabulation(vector<int>& arr){
+        int n=arr.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0)); // here we will be initializing the dp with zero not with -1 
+
+        for(int curr=n-1; curr>=0; curr--){
+            for(int prev=curr-1; prev>=-1; prev--){
+                int include=0;
+                if(prev == -1 || arr[curr]>arr[prev]){
+                    include=1+dp[curr+1][curr+1];
+                }
+                int exclude=dp[curr+1][prev+1];
+                dp[curr][prev+1]=max(include,exclude);
+            }
+        }
+        return dp[0][0]; // yaha per bhi hume shifting karni padege kyki jaha pe bhi hamara prev -1 hora raha tha humne shifting ki hai 
+    }
     int solveUsingRec(vector<int>& arr, int curr, int prev){
         // base case 
         if(curr>=arr.size()){
@@ -40,10 +56,11 @@ public:
 
         // STEP 1 AS this question requires 2 parameters to be changed in the recurrsion call so this question is of 2d type dp problem based 
 
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        // vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
 
         // int ans=solveUsingRec(nums,curr,prev);
-        int ans=solveUsingMem(nums,curr,prev,dp);
+        // int ans=solveUsingMem(nums,curr,prev,dp);
+        int ans=solveUsingTabulation(nums);
         return ans;
     }
 };
