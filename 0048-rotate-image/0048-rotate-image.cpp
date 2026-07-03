b0d825dab01d64d0021f21e718333fa00e3a117b
Abeer Pathela <pathelaabeer@gmail.com>
2026-07-03 18:25:31 +0530

    Time: 0 ms (100%), Space: 10.4 MB (7.96%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,14 +4,14 @@ public:
         int row_size=matrix.size();
         int col_size=matrix[0].size();
 
-        vector<vector<int>>answer(col_size,vector<int>(row_size));
+        vector<vector<int>>tempArray(col_size,vector<int>(row_size));
 
         for(int i=0; i<row_size; i++){
             for(int j=0; j<col_size; j++){
-                answer[j][row_size-1-i]=matrix[i][j];
+                tempArray[j][row_size-1-i]=matrix[i][j];
             }
         }
 
-        matrix=answer;
+        matrix=tempArray;
     }
 };
